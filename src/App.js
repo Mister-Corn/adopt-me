ae6d97c3b116fbd3712ee43cc4715683bd5483dc
Mister-Corn <code@ronaldlibago.com>
2018-12-18 13:09:18 -0800

    Complete first parts of course
    * Using non-JSX React
    * Setting up Prettier and ESLint
    
    Next: Parcel!

---
+++ src/App.js
@@ -0,0 +1,34 @@
+/* global React ReactDOM */ // temporary fix until you properly import
+
+// Your code is going to go here
+const Pet = props => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed)
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "dog",
+      breed: "Havanese"
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "bird",
+      breed: "Cockatiel"
+    }),
+    React.createElement(Pet, {
+      name: "Doink",
+      animal: "cat",
+      breed: "Mixed"
+    })
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
+// `createElement` can handle a string, or actual components you wrote
