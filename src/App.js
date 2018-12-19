67fc58bdbe9c9ecaeaaa78170e498522caf19a3c
Mister-Corn <code@ronaldlibago.com>
2018-12-18 21:14:57 -0800

    Update ESLint etc for JSX

--- src/App.js
+++ src/App.js
@@ -1,34 +1,42 @@
-/* global React ReactDOM */ // temporary fix until you properly import
+import React from "react";
+import { render } from "react-dom";
+import { Pet } from "./Pet";
 
-// Your code is going to go here
-const Pet = props => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed)
-  ]);
-};
+class App extends React.Component {
+  // return React.createElement("div", {}, [
+  //   React.createElement("h1", {}, "Adopt me!"),
+  //   React.createElement(Pet, {
+  //     name: "Luna",
+  //     animal: "dog",
+  //     breed: "Havanese"
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Pepper",
+  //     animal: "bird",
+  //     breed: "Cockatiel"
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Doink",
+  //     animal: "cat",
+  //     breed: "Mixed"
+  //   })
+  // ]);
 
-const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "dog",
-      breed: "Havanese"
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "bird",
-      breed: "Cockatiel"
-    }),
-    React.createElement(Pet, {
-      name: "Doink",
-      animal: "cat",
-      breed: "Mixed"
-    })
-  ]);
-};
+  handleTitleClick() {
+    alert("you clicked on the title!");
+  }
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+  render() {
+    return (
+      <div>
+        <h1>Adopt me</h1>
+        <Pet name="Luna" animal="dog" breed="Havanese" />
+        <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+        <Pet name="Doink" animal="cat" breed="Mixed" />
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
 // `createElement` can handle a string, or actual components you wrote
