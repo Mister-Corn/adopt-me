71aeb0da3096ee6fcbafc59caa2687afda748511
Mister-Corn <code@ronaldlibago.com>
2018-12-20 11:30:19 -0800

    Implement Carousel and hook it up with Details

--- src/Pet.js
+++ src/Pet.js
@@ -1,17 +1,20 @@
 import React from "react";
+import { Link } from "@reach/router";
 // Your code is going to go here
 class Pet extends React.Component {
   render() {
-    const { name, animal, breed, media, location } = this.props;
+    const { name, animal, breed, media, location, id } = this.props;
 
     let photos = [];
+
+    console.log(`media: ${media}, media.photos: ${media.photos}`);
 
     if (media && media.photos && media.photos.photo) {
       photos = media.photos.photo.filter(photo => photo["@size"] === "pn");
     }
 
     return (
-      <div className="pet">
+      <Link to={`/details/${id}`} className="pet">
         <div className="image-container">
           <img src={photos[0] ? photos[0].value : null} alt={name} />
         </div>
@@ -21,7 +24,7 @@ class Pet extends React.Component {
             {animal} - {breed} - {location}
           </h2>
         </div>
-      </div>
+      </Link>
     );
   }
 }
